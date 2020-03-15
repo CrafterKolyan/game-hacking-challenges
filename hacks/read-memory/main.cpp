60e1d8927ff1249e35370cf971e847617cae0331
Nikolay Korolev <korolevns98@gmail.com>
2020-03-15 20:52:52 +0300

    Make a function of finding process

--- hacks/read-memory/main.cpp
+++ hacks/read-memory/main.cpp
@@ -7,19 +7,26 @@
 
 constexpr char applicationName[] = "guess-the-number.exe";
 constexpr DWORD_PTR valueAddress = 0x0061F2A8; // Found using Cheat Engine 6.8.3
- 
-int main() {
+
+HANDLE find_process(const char *applicationName) {
   HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
   PROCESSENTRY32 entry;
   entry.dwSize = sizeof(PROCESSENTRY32);
 
-  HANDLE game_process = nullptr;
+  HANDLE process = nullptr;
   while (Process32Next(snapshot, &entry)) {
     if (strcmp(entry.szExeFile, applicationName) == 0) {
-      game_process = OpenProcess(PROCESS_VM_READ, FALSE, entry.th32ProcessID);
+      process = OpenProcess(PROCESS_VM_READ, FALSE, entry.th32ProcessID);
+      break;
     }
   }
   CloseHandle(snapshot);
+
+  return process;
+}
+ 
+int main() {
+  HANDLE game_process = find_process(applicationName);
 
   if (game_process == nullptr) {
     std::cout << "Start " << applicationName << " first" << std::endl;
