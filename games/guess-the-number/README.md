a7d02a9859edf38e0e2f3da051bb4f9721ec61db
Nikolay Korolev <korolevns98@gmail.com>
2020-03-15 03:06:29 +0300

    Correct guess the number

--- games/guess-the-number/README.md
+++ games/guess-the-number/README.md
@@ -1,6 +1,6 @@
 # Guess the number
 
-This game is not yet done. Current status: **not ready**
+This game is not yet done. Current status: **OK**
 
 ## Description (How to play)
 1. Type maximum possible number `max_number` to be guessed. `max_number` must be integer and be not less than `1`.
@@ -8,7 +8,7 @@ Program will generate random number in range `[0; max_number]`
 2. Type your guesses of number.  
 You will get `< (generated number is less)` if generated number is less than the one you typed in.  
 You will get `> (generated number is greater)` if generated number is greater than the one you typed in.  
-If you guess the number you will be awarded with points `(1 - <number of guesses> / log[2](max_number)) * 10` rounded by two digits after the decimal point and be asked to start the game again or quit. (`log[2](max_number)` is logarithm of `max_number` to base `2`).
+If you guess the number you will be awarded with points `(1 - <number of guesses> / log[2](max(max_number, 2))) * 10` rounded by two digits after the decimal point and be asked to start the game again or quit. (`log[2](max_number)` is logarithm of `max_number` to base `2`).
 
 Note: `log[2](max_number)` and not `log[2](max_number + 1)` in points calculation is done on purpose.
 
--- games/guess-the-number/main.cpp
+++ games/guess-the-number/main.cpp
@@ -1,15 +1,74 @@
 #include <iostream>
+#include <chrono>
 #include <string>
+#include <random>
 
-int request_int(std::string prompt_string = "") {
-  std::cout << prompt_string;
-  int x;
-  std::cin >> x;
-  return x;
+constexpr char maximumNumberRead[] = "Type maximum possible number: ";
+constexpr char guessRead[] = "Type your guess: ";
+constexpr char scoreString[] = "Score: ";
+
+constexpr char expectedIntegerNumber[] = "Expected: integer number";
+constexpr char invalidNumber[] = "Number must be not less than 0";
+constexpr char toobigNumber[] = "Too big number";
+
+constexpr char gameStart[] = "Try to guess the number";
+constexpr char gameLessValue[] = "< (generated number is less)";
+constexpr char gameGreaterValue[] = "> (generated number is greater)";
+
+unsigned long long request_ll(std::string prompt_string = "") {
+  std::string s;
+  unsigned long long value;
+  size_t pos = 0;
+  while (true) {
+    std::cout << prompt_string;
+    getline(std::cin, s);
+    try {
+      value = std::stoll(s, &pos);
+      if (pos != s.size()) {
+        std::cout << expectedIntegerNumber << std::endl;
+        continue;
+      }
+      break;
+    } catch (std::invalid_argument) {
+      std::cout << expectedIntegerNumber << std::endl;
+    } catch (std::out_of_range) {
+      std::cout << expectedIntegerNumber << std::endl;
+    }
+  }
+  return value;
+}
+
+double game(long long maximum_number) {
+  std::mt19937 generator(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+  std::uniform_int_distribution<long long> distribution(0, maximum_number);
+  long long value = distribution(generator);
+
+  std::cout << gameStart << std::endl;
+  long long guess = request_ll(guessRead);
+  unsigned long long guesses = 1;
+  while (guess != value) {
+    if (value < guess) {
+      std::cout << gameLessValue << std::endl;
+    } else {
+      std::cout << gameGreaterValue << std::endl;
+    }
+    guess = request_ll(guessRead);
+    ++guesses;
+  }
+
+  double divisor = log2(std::max(maximum_number, 2LL));
+  double score = (1 - guesses / divisor) * 10;
+
+  return score;
 }
 
 int main() {
   std::ios_base::sync_with_stdio(false);
-  int maximum_number = request_int("Type maximum possible number: ");
-  std::cout << maximum_number << std::endl;
+  long long maximum_number = request_ll(maximumNumberRead);
+  while(maximum_number <= 0) {
+    std::cout << invalidNumber << std::endl;
+    maximum_number = request_ll(maximumNumberRead);
+  }
+  double score = game(maximum_number);
+  std::cout << scoreString << score << std::endl;
 }
